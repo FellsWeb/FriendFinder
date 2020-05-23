5da2f5540758946062706afaf239f3d8de73671a
Orenn Fells <orenn.fells@gmail.com>
2020-05-23 17:12:49 -0400

    updated friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+  {
+    name: "Omar",
+    photo:
+      "https://www.hbo.com/content/dam/hbodata/series/the-wire/character/the-street/omar-little-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+    scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+  },
+  {
+    name: "Bubbles",
+    photo:
+      "https://www.hbo.com/content/dam/hbodata/series/the-wire/character/the-street/bubbles-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+    scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+  },
+  {
+    name: "McNulty",
+    photo:
+      "https://www.hbo.com/content/dam/hbodata/series/the-wire/character/the-law/james-jimmy-mcnulty-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+    scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+  },
+  {
+    name: "Stringer",
+    photo:
+      "https://www.hbo.com/content/dam/hbodata/series/the-wire/character/the-street/russell-stringer-bell-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+    scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+  },
+  {
+    name: "Avon",
+    photo:
+      "https://www.hbo.com/content/dam/hbodata/series/the-wire/character/the-street/avon-barksdale-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+    scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+  }
+];
+
+module.exports = friends;
